fe4f92eb4d92c89b66116f19ee22bd58b9ad36ed
Adele Iren Westrum Kjølstad <aikjolst@stud.ntnu.no>
2025-09-04 11:42:57 +0200

    Added simple VoteOption

---
+++ src/main/java/com/example/Experiment/VoteOption.java
@@ -0,0 +1,26 @@
+package com.example.Experiment;
+
+public class VoteOption {
+  public String getCaption() {
+    return caption;
+  }
+
+  public void setCaption(String caption) {
+    this.caption = caption;
+  }
+
+  public int getPresentationOrder() {
+    return presentationOrder;
+  }
+
+  public void setPresentationOrder(int presentationOrder) {
+    this.presentationOrder = presentationOrder;
+  }
+
+  private String caption;
+  private int presentationOrder;
+
+  public VoteOption() {};
+
+
+}
